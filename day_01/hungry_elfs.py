f627bb0a4b6a2dec658b0f9c572f3d21e520325e
Benjamin Gorjanc <benjamingorjanc@Benjamins-MacBook-Air.local>
2022-12-16 16:27:10 +0400

    First task complete.

--- day_01/hungry_elfs.py
+++ day_01/hungry_elfs.py
@@ -1,3 +1,13 @@
-file = open("./food_list.txt","r")
+file = open("food_list.txt","r")
+current_elf_calories=0
+elf_with_max_calories=0
 for line in file:
-    print(line)
+    if line.strip():
+        #Check if line is empty.
+        current_elf_calories+=int(line)
+        print("Current callories: ", current_elf_calories)
+    else:
+        if current_elf_calories>elf_with_max_calories:
+            elf_with_max_calories=current_elf_calories
+        current_elf_calories=0
+print("Elf with max callories has:", elf_with_max_calories)
